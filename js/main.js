72d48d8b6c3a2426b7b6742027aa3995ebc168e1
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-28 16:07:09 +0200

    CREATE - add new list item

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  // GET to-do List
+  // READ - "GET" to-do List
   $.ajax(
     {
       "url": "http://157.230.17.132:3001/todos",
@@ -12,7 +12,36 @@ $(document).ready(function(){
         alert("Errore");
       }
     }
-  )
+  );
+
+  // CREATE - "POST" new element in to-do List
+  $("#add").click(
+    function(){
+      // Get input value
+      var itemText = $("#new-item").val();
+      // console.log(itemText);
+      if (itemText != "") {
+        $.ajax(
+          {
+            "url": "http://157.230.17.132:3001/todos",
+            "method": "POST",
+            "data": {
+              "text": itemText
+            },
+            "success": function(data){
+              var itemArray = [];
+              itemArray.push(data)
+              //console.log(data);
+              renderList(itemArray);
+            },
+            "error": function(error){
+              alert("Errore");
+            }
+          }
+        )
+      }
+    }
+  );
 });
 
 // FUNCTION - render to-do List
@@ -25,5 +54,4 @@ function renderList(results){
     var html = template(results[i]);
     $("#to-do-list").append(html);
   }
-
-}
+};
