06f16569704f802fb746faadaab4cbea337ede2c
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-28 22:02:55 +0200

    Refactoring + style

--- js/main.js
+++ js/main.js
@@ -17,28 +17,14 @@ $(document).ready(function(){
   // CREATE - "POST" new element in to-do List
   $("#add").click(
     function(){
-      // Get input value
-      var itemText = $("#new-item").val();
-      // console.log(itemText);
-      if (itemText != "") {
-        $.ajax(
-          {
-            "url": "http://157.230.17.132:3001/todos",
-            "method": "POST",
-            "data": {
-              "text": itemText
-            },
-            "success": function(data){
-              var itemArray = [];
-              itemArray.push(data)
-              //console.log(data);
-              renderList(itemArray);
-            },
-            "error": function(error){
-              alert("Errore");
-            }
-          }
-        )
+      addItem();
+    }
+  );
+
+  $("#new-item").keypress(
+    function(){
+      if (event.which == 13) {
+        addItem();
       }
     }
   );
@@ -46,7 +32,7 @@ $(document).ready(function(){
   // DELETE - "DELETE" element from to-do list
   $("#to-do-list").on("click", ".far.fa-trash-alt",
     function(){
-      var listElement = $(this).parent();
+      var listElement = $(this).parents(".list-item");
       //console.log(listElement);
       var id = listElement.attr("id");
       //console.log(id);
@@ -72,35 +58,22 @@ $(document).ready(function(){
     function(){
       $(".modal").show();
       // Get list item id
-      var id = $(this).parent().attr("id");
+      var id = $(this).parents(".list-item").attr("id");
       // console.log(id);
-      var textToEdit = $(this).siblings(".item-text")
+      var textToEdit = $(this).parent().siblings(".item-text")
       // console.log(textToEdit);
 
       //UPDATE - "PATCH" edit element list
       $("#edit").click(
         function(){
-          var editedText = $("#edit-item").val();
-          // console.log(editedText);
-          if (editedText != "") {
-            $.ajax(
-              {
-                "url": "http://157.230.17.132:3001/todos/" + id,
-                "method": "PATCH",
-                "data": {
-                  "text": editedText,
-                },
-                "success": function(data){
-                  // console.log(data);
-                  textToEdit.text(editedText);
-                },
-                "error": function(error){
-                  alert("Errore");
-                }
-              }
-            );
-            // Close modal
-            $(".modal").hide();
+          editItem();
+        }
+      )
+
+      $("#edit-item").keypress(
+        function(){
+          if (event.which == 13) {
+            editItem();
           }
         }
       )
@@ -124,5 +97,58 @@ function renderList(results){
   for (var i = 0; i < results.length; i++) {
     var html = template(results[i]);
     $("#to-do-list").append(html);
+  }
+};
+
+// FUNCTION - add list item
+function addItem(){
+  // Get input value
+  var itemText = $("#new-item").val();
+  // console.log(itemText);
+  if (itemText != "") {
+    $.ajax(
+      {
+        "url": "http://157.230.17.132:3001/todos",
+        "method": "POST",
+        "data": {
+          "text": itemText
+        },
+        "success": function(data){
+          var itemArray = [];
+          itemArray.push(data)
+          //console.log(data);
+          renderList(itemArray);
+        },
+        "error": function(error){
+          alert("Errore");
+        }
+      }
+    );
+    itemText = "";
+  }
+};
+// FUNCTION - edit list item
+function editItem(){
+  var editedText = $("#edit-item").val();
+  // console.log(editedText);
+  if (editedText != "") {
+    $.ajax(
+      {
+        "url": "http://157.230.17.132:3001/todos/" + id,
+        "method": "PATCH",
+        "data": {
+          "text": editedText,
+        },
+        "success": function(data){
+          // console.log(data);
+          textToEdit.text(editedText);
+        },
+        "error": function(error){
+          alert("Errore");
+        }
+      }
+    );
+    // Close modal
+    $(".modal").hide();
   }
 };
